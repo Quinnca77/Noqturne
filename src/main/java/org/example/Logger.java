3f17d8602aadbee528e4989d58abab806a7b8ace
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2024-09-11 16:07:40 +0200

    Documented Logger class

--- src/main/java/org/example/Logger.java
+++ src/main/java/org/example/Logger.java
@@ -1,18 +1,34 @@
 package org.example;
 
+/**
+ * Singleton class for handling all status updates in the program.
+ */
 public class Logger {
     private final guiTagger gui;
     private static Logger logger;
 
+    /**
+     * This constructor is never called except for the initialization of guiTagger.
+     * Keep it like that.
+     * @param gui the GUI for the logger to attach to
+     */
     public Logger(guiTagger gui) {
         this.gui = gui;
         logger = this;
     }
 
+    /**
+     * @return an instance of logger meant for viewing status updates to the user.
+     */
     public static Logger getLogger() {
         return logger;
     }
 
+    /**
+     * Prints a string to the GUI for the user to see. Only meant for user-friendly
+     * language, no technical specifics.
+     * @param string the string to be displayed to the user via the GUI textbox
+     */
     public void println(String string) {
         gui.displayText(string + "\n");
     }
