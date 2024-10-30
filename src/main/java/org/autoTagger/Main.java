b21cd686ff2d97bedd8de4fc79a099f41d18fea7
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2024-10-30 13:28:30 +0100

    Added TODO

--- src/main/java/org/autoTagger/Main.java
+++ src/main/java/org/autoTagger/Main.java
@@ -2,6 +2,7 @@ package org.autoTagger;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 
+// TODO add tests with songs online that cover every functionality
 // TODO rework UI with tabs, keep log screen at the same place but make three tabs for each functionality
 public class Main {
     public static void main(String[] args) {
