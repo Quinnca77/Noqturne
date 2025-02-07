17f72c87876b6ab6b9335e9e1b9fb66d4b420172
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2025-02-07 12:39:53 +0100

    Deleted TODO

--- src/main/java/org/autoTagger/Main.java
+++ src/main/java/org/autoTagger/Main.java
@@ -3,7 +3,6 @@ package org.autoTagger;
 import com.formdev.flatlaf.FlatDarkLaf;
 
 // TODO make directory choice user for downloaded songs
-// TODO use a different way of selecting an mp3 file instead of a filepath
 public class Main {
     public static void main(String[] args) {
         FlatDarkLaf.setup();
