db6d5beba92dcf8aa0f89b26c22f28057bba371e
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2025-04-19 15:02:39 +0200

    Make tests headless-compatible

--- src/test/java/org/autoTagger/GuiTaggerTest.java
+++ src/test/java/org/autoTagger/GuiTaggerTest.java
@@ -1,9 +1,6 @@
 package org.autoTagger;
 
-import com.mpatric.mp3agic.ID3v2;
-import com.mpatric.mp3agic.InvalidDataException;
-import com.mpatric.mp3agic.Mp3File;
-import com.mpatric.mp3agic.UnsupportedTagException;
+import com.mpatric.mp3agic.*;
 import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
@@ -11,9 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import javax.swing.*;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.StyledDocument;
 import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
@@ -23,15 +17,12 @@ import java.nio.file.Path;
 import java.util.Objects;
 
 import static org.autoTagger.Tagger.PATH_TO_SONGS;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
 
 class GuiTaggerTest {
 
-    private GuiTagger guiTagger;
-    private JTextField mockFilePathSong;
-    private JTextField mockVIdThumbnail;
+    private Tagger tagger;
+    private Downloader downloader;
     private static final String TEST_SONG_URL = "https://www.youtube.com/watch?v=U0TXIXTzJEY";
     private static final String TEST_SONG_NAME = "il vento d'oro.mp3";
     private static final String TEST_SONG_ARTIST = "Yugo Kanno";
@@ -40,41 +31,26 @@ class GuiTaggerTest {
     private static final String TEST_COVER_ART = "CoverArt.jpg";
 
     @BeforeEach
-    void setUp() throws BadLocationException {
-        guiTagger = spy(new GuiTagger(true));
+    void setUp() {
 
-        // Mock dependencies
-        JTextField mockSongPlaylistURLTextField = Mockito.mock(JTextField.class);
-        mockFilePathSong = Mockito.mock(JTextField.class);
-        mockVIdThumbnail = Mockito.mock(JTextField.class);
-        JTextPane mockLoadingText = Mockito.mock(JTextPane.class);
-        StyledDocument mockStyledDocument = Mockito.mock(StyledDocument.class);
+        GuiTagger mockGui = Mockito.mock(GuiTagger.class);
+        Logger mockLogger = new Logger(mockGui);
+        this.tagger = new Tagger();
+        this.downloader = new Downloader();
 
-        // Set mock objects in guiTagger
-        guiTagger.songPlaylistURLTextField = mockSongPlaylistURLTextField;
-        guiTagger.filePathSong = mockFilePathSong;
-        guiTagger.vIdThumbnail = mockVIdThumbnail;
-        guiTagger.loadingText = mockLoadingText;
+        Mockito.doNothing().when(mockGui).displayText(anyString());
 
-        Mockito.doNothing().when(guiTagger).showMD(any(), anyString());
-        guiTagger.renameState = false;
-        when(mockLoadingText.getStyledDocument()).thenReturn(mockStyledDocument);
-        Mockito.doNothing().when(mockStyledDocument).insertString(anyInt(), anyString(), any());
     }
 
     @Test
-    void individualTagTest() throws IOException, InvalidDataException, UnsupportedTagException {
+    void individualTagTest() throws IOException, InvalidDataException, UnsupportedTagException, NotSupportedException {
         // Copy test mp3 file to Downloads folder to test application
         File copied = copyTestFileToTagFolder();
-
-        // Set up mock behavior
-        when(mockFilePathSong.getText()).thenReturn(PATH_TO_SONGS + TEST_SONG_ARTIST + " - " + TEST_SONG_TITLE + ".mp3");
-        when(mockVIdThumbnail.getText()).thenReturn(TEST_SONG_VID);
 
         copied = Files.move(copied.toPath(), copied.toPath().resolveSibling(TEST_SONG_ARTIST + " - " + TEST_SONG_TITLE + ".mp3")).toFile();
 
         // Test if mp3 file is as expected after method is called
-        guiTagger.addCoverForIndividualFile();
+        tagger.tagIndividualFile(PATH_TO_SONGS + TEST_SONG_ARTIST + " - " + TEST_SONG_TITLE + ".mp3", TEST_SONG_VID);
         testResultingFile();
 
         // Clean up
@@ -84,13 +60,13 @@ class GuiTaggerTest {
     }
 
     @Test
-    void batchTagTest() throws InvalidDataException, UnsupportedTagException, IOException {
+    void batchTagTest() throws InvalidDataException, UnsupportedTagException, IOException, NoSongFoundException, NotSupportedException, InterruptedException {
         // Copy test mp3 file to Downloads folder to test application
         File copied = copyTestFileToTagFolder();
         copied = Files.move(copied.toPath(), copied.toPath().resolveSibling(TEST_SONG_ARTIST + " - " + TEST_SONG_TITLE + ".mp3")).toFile();
 
         // Test if mp3 file is as expected after method is called
-        guiTagger.tagAllFiles(new File[]{copied});
+        tagger.tagAllFiles(new File[]{copied});
         testResultingFile();
 
         // Clean up
@@ -100,14 +76,14 @@ class GuiTaggerTest {
     }
 
     @Test
-    void downloadAndTagTest() throws InvalidDataException, UnsupportedTagException, IOException, InterruptedException {
-        new Downloader().downloadSongs(TEST_SONG_URL);
+    void downloadAndTagTest() throws InvalidDataException, UnsupportedTagException, IOException, InterruptedException, NoSongFoundException, NotSupportedException {
+        downloader.downloadSongs(TEST_SONG_URL);
 
         // Test if mp3 file is as expected after method is called
         File song = new File(PATH_TO_SONGS + TEST_SONG_TITLE + ".mp3");
         Path songPath = song.toPath();
         song = Files.move(songPath, songPath.resolveSibling(TEST_SONG_ARTIST + " - " + TEST_SONG_TITLE + ".mp3")).toFile();
-        guiTagger.tagAllFiles(new File[]{song});
+        tagger.tagAllFiles(new File[]{song});
         testResultingFile();
 
         // Clean up
