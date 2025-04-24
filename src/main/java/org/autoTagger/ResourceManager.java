09878ff1514e6c9c07e0f205295ddc666cddb96f
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2025-04-24 13:49:35 +0200

    Document ResourceManager

--- src/main/java/org/autoTagger/ResourceManager.java
+++ src/main/java/org/autoTagger/ResourceManager.java
@@ -21,10 +21,22 @@ public class ResourceManager {
     private static final String PY_FILE_SUFFIX = ".py";
     private static Path tempPyFilePath;
 
+    /**
+     * Gets the Path to the yt-dlp binary used for downloading songs.
+     * The binary will be located in its own folder in %APPDATA%/Roaming, but if it does not
+     * exist there yet, it will download it promptly.
+     * This method also checks whether the ffmpeg binaries needed for the functionality of yt-dlp
+     * are present.
+     *
+     * @return Path object pointing directly to the yt-dlp binary
+     * @throws IOException if an I/O errors occurs
+     */
     public static Path getYtDlpPath() throws IOException {
         Path binDir = appDir.resolve("bin");
         Files.createDirectories(binDir);
         Path ytDlpPath = binDir.resolve("yt-dlp.exe");
+
+        // If yt-dlp has not previously been copied to %APPDATA%/Roaming
         if (!Files.exists(ytDlpPath)) {
             logger.println("yt-dlp dependency not found, downloading now...");
             downloadFromUrl("https://github.com/yt-dlp/yt-dlp/releases/download/2025.03.31/yt-dlp.exe", ytDlpPath);
@@ -48,6 +60,7 @@ public class ResourceManager {
             }
         }
 
+        // Checks whether ffmpeg dependencies are present
         Path ffmpegDependencyPath = binDir.resolve("ffmpeg.exe");
         if (!Files.exists(ffmpegDependencyPath)) {
             logger.println("ffmpeg not found, downloading now...");
@@ -56,17 +69,20 @@ public class ResourceManager {
                     ffmpegZipPath);
             logger.println("ffmpeg.zip downloaded!");
 
+            // Unzips downloaded zip file
             logger.println("Unzipping...");
             Path ffmpegUnzippedDirectory = binDir.resolve("ffmpeg_temp");
             unzip(ffmpegZipPath.toString(), ffmpegUnzippedDirectory.toString());
             logger.println("Unzipped!");
 
+            // Move files from extracted zip to desired location for yt-dlp
             logger.println("Moving files...");
             searchAndMoveFile("ffmpeg.exe", ffmpegUnzippedDirectory, binDir.resolve("ffmpeg.exe"));
             searchAndMoveFile("ffprobe.exe", ffmpegUnzippedDirectory, binDir.resolve("ffprobe.exe"));
             searchAndMoveFile("ffplay.exe", ffmpegUnzippedDirectory, binDir.resolve("ffplay.exe"));
             logger.println("Moving files successful!");
 
+            // Deleting files that will not be used anymore
             logger.println("Removing remnants...");
             Files.delete(ffmpegZipPath);
             FileUtils.deleteDirectory(ffmpegUnzippedDirectory.toFile());
@@ -75,13 +91,31 @@ public class ResourceManager {
         return ytDlpPath;
     }
 
+    /**
+     * Downloads file from a website to a specified filepath
+     *
+     * @param url website to download from. Should directly point to the download link
+     * @param path Path object to download the file to
+     * @throws IOException if an I/O error occurs
+     */
     private static void downloadFromUrl(String url, Path path) throws IOException {
-        URL ytDlpUrl = new URL(url);
-        try (InputStream in = ytDlpUrl.openStream()) {
+        URL urlObject = new URL(url);
+        try (InputStream in = urlObject.openStream()) {
             Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
         }
     }
 
+    /**
+     * Slightly modified version of the unzip function found on
+     * <a href="https://www.geeksforgeeks.org/how-to-zip-and-unzip-files-in-java/">GeeksForGeeks</a>
+     * Used to unzip the ffmpeg zip folder after it is downloaded, but can be used to unzip
+     * any arbitrary zip file.
+     *
+     * @param zipFile String representing the file path to the zip file
+     * @param destFolder String representing the file path to the destination folder where the
+     *                   zip file will get unzipped to
+     * @throws IOException if an I/O error occurs
+     */
     @SuppressWarnings("SameParameterValue")
     private static void unzip(String zipFile, String destFolder) throws IOException {
         try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile))) {
@@ -111,6 +145,15 @@ public class ResourceManager {
         }
     }
 
+    /**
+     * Uses an iterative approach to search for a specified file in a specified folder and
+     * will move it to a specified path after finding it.
+     *
+     * @param fileName String denoting the filename of the file to be found
+     * @param pathToSearch Path object representing the folder to search in
+     * @param pathToMove After finding the file, moves it to this specified Path
+     * @throws IOException if an I/O errors occurs
+     */
     private static void searchAndMoveFile(String fileName, Path pathToSearch, Path pathToMove) throws IOException {
         File f = pathToSearch.toFile();
         Stack<File> stack = new Stack<>();
@@ -135,10 +178,14 @@ public class ResourceManager {
         }
     }
 
+    /**
+     * Makes sure ytmusicapi is pip installed. If it isn't, it will automatically install it and log the results.
+     */
     public static void ensureYtMusicApiInstallation() {
         try {
             Process process = new ProcessBuilder("python", "-m", "pip", "show", "ytmusicapi").start();
             int exitCode = process.waitFor();
+            // If pip show returns an error, ytmusicapi is not installed yet
             if (exitCode != 0) {
                 logger.println("Couldn't find Python dependency ytmusicapi, installing now...");
                 process = new ProcessBuilder("python", "-m", "pip", "install", "ytmusicapi").start();
@@ -155,6 +202,15 @@ public class ResourceManager {
         }
     }
 
+    /**
+     * Gets the Path to the coverArt.py file used for getting the vId of a song.
+     * This file is located in the resources folder but for a compiled jar we have
+     * to first extract it to a temporary file from which we can invoke it from the
+     * command line.
+     *
+     * @return Path to coverArt.py file
+     * @throws IOException if an I/O error occurs
+     */
     public static Path getCoverArtPy() throws IOException {
         if (tempPyFilePath == null) {
             InputStream in = ResourceManager.class.getResourceAsStream(PY_FILE);
@@ -163,6 +219,7 @@ public class ResourceManager {
                 throw new FileNotFoundException(PY_FILE);
             }
             File tempPyFile = File.createTempFile(PY_FILE_PREFIX, PY_FILE_SUFFIX);
+            // Once Auto-Tagger exits, this temporary python file gets deleted
             tempPyFile.deleteOnExit();
             Path tempPath = tempPyFile.toPath();
             Files.copy(in, tempPath, StandardCopyOption.REPLACE_EXISTING);
