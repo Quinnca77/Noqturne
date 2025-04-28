7a111880a51f9384111ad7c3793ea96e77ed1bac
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2025-04-28 16:48:09 +0200

    Fix bug

--- src/main/java/org/autoTagger/ErrorLogger.java
+++ src/main/java/org/autoTagger/ErrorLogger.java
@@ -12,16 +12,23 @@ public class ErrorLogger {
     /**
      * Called when a critical Exception occurs. Dumps the stack trace of the Exception to a log
      * file for debugging purposes.
+     * Also calls {@link GuiTagger#displayText(String, boolean)} to tell the user an error occurred.
+     * <p>
+     * When calling this function, be sure not to duplicate the error logging to the UI console!
      *
      * @param e the Exception to be logged.
      */
     public static void runtimeExceptionOccurred(Throwable e) {
         logToFile(e);
+        Logger.getLogger().printError("A runtime error occurred, check errorLog.log for more details");
     }
 
     /**
      * Called when a critical error occurs. Dumps the details of the error to a log file
      * for debugging purposes.
+     * Also calls {@link GuiTagger#displayText(String, boolean)} to tell the user an error occurred.
+     * <p>
+     * When calling this function, be sure not to duplicate the error logging to the UI console!
      *
      * @param text the description of the error that occurred.
      */
